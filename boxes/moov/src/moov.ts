4497845af002012044ba6bd1f3033092c6de8d60
Bradley Odell <btodell@hotmail.com>
2021-07-27 17:45:15 -0700

    Implemented tkhd box parsing. Fixed bug in mvhd box parsing.

--- boxes/moov/src/moov.ts
+++ boxes/moov/src/moov.ts
@@ -2,14 +2,16 @@ import {BoxEncoding} from "@isomp4/core";
 import {hdlr} from "./hdlr.js";
 import {mvhd} from "./mvhd.js";
 import {stsd} from "./stsd.js";
+import {tkhd} from "./tkhd.js";
 
 export const stbl = new BoxEncoding("stbl", stsd);
 export const minf = new BoxEncoding("minf", stbl);
 export const mdia = new BoxEncoding("mdia", hdlr, minf);
-export const trak = new BoxEncoding("trak", mdia);
+export const trak = new BoxEncoding("trak", tkhd, mdia);
 export const moov = new BoxEncoding("moov", mvhd, trak);
 
 export * from "./hdlr.js";
 export * from "./mvhd.js";
 export * from "./samples/avc.js";
 export * from "./stsd.js";
+export * from "./tkhd.js";
--- boxes/moov/src/tkhd.ts
+++ boxes/moov/src/tkhd.ts
@@ -10,28 +10,50 @@ import {
     readSafeUInt64BE,
 } from "@isomp4/core";
 
-export interface MovieHeaderBox extends FullBox {
+export interface TrackHeaderBox extends FullBox {
     creationTime: Date;
     modificationTime: Date;
-    timescale: number;
+    trackID: number;
     duration: number;
-    rate: number;
+    layer: number;
+    alternateGroup: number;
     volume: number;
     matrix: number[];
-    nextTrackID: number;
+    width: number;
+    height: number;
 }
 
-class MovieHeaderBoxEncoding extends FullBoxEncoding {
+export namespace TrackHeaderBox {
 
-    constructor() {
-        super("mvhd");
+    export interface Flags {
+        enabled: boolean;
+        inMovie: boolean;
+        inPreview: boolean;
+        sizeIsAspectRatio: boolean;
     }
 
-    public override encodingLength(obj: MovieHeaderBox): number {
+    export function parseFlags(flags: number): Flags {
+        return {
+            enabled: (flags & 0x000001) !== 0,
+            inMovie: (flags & 0x000002) !== 0,
+            inPreview: (flags & 0x000004) !== 0,
+            sizeIsAspectRatio: (flags & 0x000008) !== 0,
+        };
+    }
+
+}
+
+class TrackHeaderBoxEncoding extends FullBoxEncoding {
+
+    constructor() {
+        super("tkhd");
+    }
+
+    public override encodingLength(obj: TrackHeaderBox): number {
         return super.encodingLength(obj); // TODO implement
     }
 
-    public override encodeTo(obj: MovieHeaderBox, buffer: Buffer): number {
+    public override encodeTo(obj: TrackHeaderBox, buffer: Buffer): number {
         const requiredBytes = super.encodeTo(obj, buffer);
         if (requiredBytes > 0) {
             return requiredBytes;
@@ -42,70 +64,78 @@ class MovieHeaderBoxEncoding extends FullBoxEncoding {
         return 0;
     }
 
-    public override decode(buffer: Buffer, header?: BoxHeader): MovieHeaderBox | number {
+    public override decode(buffer: Buffer, header?: BoxHeader): TrackHeaderBox | number {
         const superBox = super.decode(buffer, header);
         if (typeof superBox === "number") {
             return superBox;
         }
         let creationTime: Date;
         let modificationTime: Date;
-        let timescale: number;
+        let trackID: number;
         let duration: number;
         let offset: number = this.decodedBytes;
         if (superBox.version === 1) {
-            // 64-bit times
-            if (buffer.length < offset + 28) {
-                return offset + 28;
+            // 64-bit times and duration
+            if (buffer.length < offset + 32) {
+                return offset + 32;
             }
             creationTime = readDate64(buffer, offset);
             offset += 8;
             modificationTime = readDate64(buffer, offset);
             offset += 8;
-            timescale = buffer.readUInt32BE(offset);
+            trackID = buffer.readUInt32BE(16);
             offset += 4;
+            offset += 4; // reserved = 0
             duration = readSafeUInt64BE(buffer, offset);
             offset += 8;
         } else { // version = 0
-            // 32-bit times
-            if (buffer.length < offset + 16) {
-                return offset + 16;
+            // 32-bit times and duration
+            if (buffer.length < offset + 20) {
+                return offset + 20;
             }
             creationTime = readDate(buffer, offset);
             offset += 4;
             modificationTime = readDate(buffer, offset);
             offset += 4;
-            timescale = buffer.readUInt32BE(offset);
+            trackID = buffer.readUInt32BE(offset);
             offset += 4;
+            offset += 4; // reserved = 0
             duration = buffer.readUInt32BE(offset);
             offset += 4;
         }
         if (buffer.length < offset + 80) {
             return offset + 80;
         }
-        const rate: number = readFixed16x16(buffer, offset);
-        offset += 4;
+        offset += 8; // reserved = 0
+        const layer: number = buffer.readInt16BE(offset);
+        offset += 2;
+        const alternateGroup: number = buffer.readInt16BE(offset);
+        offset += 2;
         const volume: number = readFixed8x8(buffer, offset);
         offset += 2;
-        offset += 10; // skip reserved (2 + 4*2)
+        offset += 2; // reserved = 0
         const matrix: number[] = readMatrix(buffer, offset, 9);
         offset += 36; // 4 * 9
-        offset += 24; // skip predefined (4*6)
-        const nextTrackID: number = buffer.readUInt32BE(offset);
+        const width: number = readFixed16x16(buffer, offset);
+        offset += 4;
+        const height: number = readFixed16x16(buffer, offset);
         offset += 4;
         this.decodedBytes = offset;
         return {
             ...superBox,
             creationTime,
             modificationTime,
-            timescale,
+            trackID,
             duration,
-            rate,
+            layer,
+            alternateGroup,
             volume,
             matrix,
-            nextTrackID,
+            width,
+            height,
         };
     }
 
 }
 
-export const mvhd = new MovieHeaderBoxEncoding();
+export const tkhd = new TrackHeaderBoxEncoding();
